f97d157113612a2be4e3fb88ff4a3507e5be7c55
disto-stack <juan.ocampo.2501@gmail.com>
2023-06-25 15:16:01 -0500

    chore(app): install babel and babel-loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import './sass/index.scss';
+const object = {
+	name: 'Juan',
+	lastName: 'Ocampo',
+};
+
+const { lastName } = object;
+
+console.log(lastName);
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,16 @@ module.exports = {
 				test: /\.s[ac]ss$/i,
 				use: ['style-loader', 'css-loader', 'sass-loader'],
 			},
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
 		],
 	},
 	plugins: [new HtmlWebpackPlugin()],
